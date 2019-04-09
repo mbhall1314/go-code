aeb7daa7e57636d01483026b8fc065946df6237e
Martin <48673824+mbhall1314@users.noreply.github.com>
2019-04-09 22:02:14 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,143 @@
+// sufix project main.go
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var sufix string
+	fmt.Scanln(&sufix)
+	postfix := sufixtopostfix(sufix)
+	answer := calculate(postfix)
+	fmt.Println(answer)
+}
+
+func sufixtopostfix(sufix string) string {
+	var postfix string
+	var stack [50]string
+	var prechar string
+	top := -1
+
+	suflen := len(sufix)
+
+	for i := 0; i < suflen; i++ {
+		char := string(sufix[i])
+
+		switch char {
+
+		//左括号进栈
+		case "(":
+			top++
+			stack[top] = "("
+
+		//右括号出栈
+		case ")":
+			for top != -1 {
+				prechar = stack[top]
+				top--
+				if prechar == "(" {
+					break
+				}
+				postfix += prechar
+			}
+
+		//number
+		case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
+			postfix += char
+			postfix += "#"
+
+		//add or sub
+		case "+", "-":
+			for top != -1 {
+				if stack[top] != "(" {
+					prechar = stack[top]
+					postfix += prechar
+					top--
+				} else {
+					break
+				}
+			}
+			top++
+			stack[top] = char
+		//mui or div
+		case "*", "/":
+			for top != -1 {
+				prechar = stack[top]
+				if prechar == "*" || prechar == "/" {
+					postfix += prechar
+					top--
+				} else {
+					break
+				}
+			}
+			top++
+			stack[top] = char
+		}
+	}
+	//if stack is not empty
+	for top != -1 {
+		prechar = stack[top]
+		postfix += prechar
+		top--
+	}
+	return postfix
+}
+
+func calculate(postfix string) float32 {
+	var stack [50]float32
+	top := -1
+	var a, b, c, d, e float32
+	var prechar string
+	strlen := len(postfix)
+
+	for i := 0; i < strlen; i++ {
+		char := string(postfix[i])
+		switch char {
+		case "+":
+			a = stack[top]
+			top--
+			b = stack[top]
+			top--
+			c = a + b
+			top++
+			stack[top] = c
+		case "-":
+			a = stack[top]
+			top--
+			b = stack[top]
+			top--
+			c = b - a
+			top++
+			stack[top] = c
+		case "*":
+			a = stack[top]
+			top--
+			b = stack[top]
+			top--
+			c = a * b
+			top++
+			stack[top] = c
+		case "/":
+			a = stack[top]
+			top--
+			b = stack[top]
+			top--
+			c = b / a
+			top++
+			stack[top] = c
+		case "#":
+			d1, _ := strconv.Atoi(prechar)
+			d = float32(d1)
+			top++
+			stack[top] = d
+			prechar = ""
+		default:
+			prechar += char
+		}
+	}
+	e = stack[top]
+	top--
+	return e
+}
